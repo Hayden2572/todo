1ffe802f74e2a220fad543f518c44c963639e9c0
Hayden2572 <nikitaborisovv353@gmail.com>
2025-04-13 21:02:02 +0300

    reset commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	todo "ToDo"
-	"ToDo/pkg/handler"
-	"ToDo/pkg/repository"
-	"ToDo/pkg/service"
-	"log"
-
-	"github.com/spf13/viper"
-)
-
 func main() {
-	if err := initConfig(); err != nil {
-		log.Fatalf("Error initializing config: %s", err.Error())
-	}
 
-	repos := repository.NewRepository()
-	services := service.NewService(repos)
-	handlers := handler.NewHandler(services)
-
-	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("Error occured while rinning http server: %s", err.Error())
-	}
-}
-
-func initConfig() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
 }
