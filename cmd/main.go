33e9ecadfb3a0ec6fea89d4fada3daae2126859e
Hayden2572 <nikitaborisovv353@gmail.com>
2025-04-13 10:18:02 +0300

    yml-read

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,30 @@ package main
 import (
 	todo "ToDo"
 	"ToDo/pkg/handler"
+	"ToDo/pkg/repository"
+	"ToDo/pkg/service"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
-	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while rinning http server: %s", err.Error())
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing config: %s", err.Error())
 	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("Error occured while rinning http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
